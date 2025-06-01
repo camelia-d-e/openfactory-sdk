f498808121fbac6190b596b098bcb6624263aaec
camelia-d-e <cades413@ulaval.ca>
2025-06-01 17:12:20 +0000

    follow openfactory-sdk doc

--- openfactory_sdk/__init__.py
+++
@@ -1,4 +0,0 @@
-""" OpenFactory-SDK module. """
-
-# sub-commands
-import openfactory_sdk.device as device
--- openfactory_sdk/device/__init__.py
+++
@@ -1,4 +0,0 @@
-""" openfactory-sdk device commands. """
-
-from openfactory_sdk.device.up import click_up
-from openfactory_sdk.device.down import click_down
