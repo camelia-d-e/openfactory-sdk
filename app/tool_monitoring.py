7215f4d212eaefe28923dafaeb45ce61163761a0
camelia-d-e <cades413@ulaval.ca>
2025-06-04 20:16:43 +0000

    Add condition logic to tool monitoring app

---
+++ app/tool_monitoring.py
@@ -0,0 +1,144 @@
+import os
+import time
+import csv
+from openfactory.apps import OpenFactoryApp
+from openfactory.kafka import KSQLDBClient
+from openfactory.assets import Asset, AssetAttribute
+
+
+
+class ToolMonitoring(OpenFactoryApp):
+    """
+    SpindleMonitoring application for monitoring the state of tools in an IVAC system.
+    Inherits from `OpenFactoryApp` and extends it to represent a specific application
+    that monitors the state of tools in an IVAC system, updating their conditions based on events.
+    Class Attributes:
+        IVAC_SYSTEM_UUID (str): Unique identifier for the IVAC system, defaults to 'IVAC'.
+    Instance Attributes:
+        tool_states (dict): Dictionary to hold the states of tools in the IVAC system.
+        ivac (Asset): Asset instance representing the IVAC system.
+    """
+
+
+    IVAC_SYSTEM_UUID: str = os.getenv('IVAC_SYSTEM_UUID', 'IVAC')
+
+    def __init__(self, app_uuid, ksqlClient, bootstrap_servers, loglevel= 'INFO'):
+        """
+        Initializes the SpindleMonitoring application.
+        Sets up the application with the provided UUID, KSQLDB client, and Kafka bootstrap servers.
+        Args:
+            app_uuid (str): Unique identifier for the application.
+            ksqlClient: KSQLDB client instance for interacting with KSQLDB.
+            bootstrap_servers (str): Comma-separated list of Kafka bootstrap servers.
+            loglevel (str): Logging level for the application. Defaults to 'INFO'.
+        """
+        super().__init__(app_uuid, ksqlClient, bootstrap_servers, loglevel)
+        self.tool_states = {}
+
+        self.add_attribute('ivac_system', AssetAttribute(
+            self.IVAC_SYSTEM_UUID,
+            type='Events',
+            tag='DeviceUuid'))
+
+
+        self.ivac= Asset(self.IVAC_SYSTEM_UUID,
+                          ksqlClient=ksqlClient,
+                          bootstrap_servers=bootstrap_servers)
+
+        self.ivac.add_attribute('ivac_condition',
+                                AssetAttribute('UNAVAILABLE',
+                                               type='Condition',
+                                               tag='SYSTEM'))
+        
+        self.ivac.subscribe_to_events(self.on_event, 'ivac_events_group')
+
+    def app_event_loop_stopped(self) -> None:
+        """
+        Called automatically when the main application event loop is stopped.
+
+        This method handles cleanup tasks such as stopping the temperature
+        sensor's sample subscription to ensure a graceful shutdown.
+        """
+        print("Stopping Temperature sensor consumer thread ...")
+        self.ivac.stop_events_subscription()
+
+    def main_loop(self) -> None:
+        """ Main loop of the App. """
+        while True:
+            time.sleep(1)
+
+    
+    def on_event(self, msg_key:str, msg_value:dict) -> None:
+        """
+        Callback for handling new events from the ivac system.
+
+        Verifies the state of the tools of the ivac system and updates
+        the ivac_condition attribute based on these conditions:
+        - If all tools are in 'ON' state, sets ivac_condition to 'ERROR'..
+        - If any tool is in 'UNAVAILABLE' state, sets ivac_condition to 'UNAVAILABLE'.
+        - Else, sets ivac_condition to 'OK'.
+
+        Writes the event data to a CSV file named 'ivac_events.csv'.
+
+        Args:
+            msg_key (str): The key of the Kafka message (the sensor ID).
+            msg_value (dict): The message payload containing sample data.
+                              Expected keys: 'id' (str), 'value' (float or str).
+        """
+        if self.tool_states.get(msg_value['id'], 0) == 0:
+            self.tool_states[msg_value['id']] = msg_value['value']
+
+        self.verify_tool_states()
+        
+        self.write_event_to_csv(msg_key, msg_value)
+
+    def verify_tool_states(self) -> None:
+        """
+        Verifies the state of the tools and updates the ivac_condition attribute.
+
+        This method checks the states of the tools in the ivac system and updates
+        the ivac_condition attribute based on the following conditions:
+        - If all tools are in 'ON' state, sets ivac_condition to 'ERROR'.
+        - If any tool is in 'UNAVAILABLE' state, sets ivac_condition to 'UNAVAILABLE'.
+        - Else, sets ivac_condition to 'OK'.
+
+        Args:
+            tool_states (dict): A dictionary containing tool states with tool IDs as keys.
+        """
+        if all(state == 'ON' for state in self.tool_states.values()) and len(self.tool_states) > 1:
+            self.ivac.ivac_condition = 'ERROR'
+        elif any(state == 'UNAVAILABLE' for state in self.tool_states.values()):
+            self.ivac.ivac_condition = 'UNAVAILABLE'
+        else:
+            self.ivac.ivac_condition = 'OK'
+
+    def write_event_to_csv(self, msg_key: str, msg_value: dict) -> None:
+        """
+        Writes the event data to a CSV file named 'ivac_events.csv'.
+
+        This method appends the event data to the CSV file, creating the file
+        if it does not exist. It ensures that the header is written only once.
+
+        Args:
+            msg_key (str): The key of the Kafka message (the sensor ID).
+            msg_value (dict): The message payload containing sample data.
+                              Expected keys: 'id' (str), 'value' (float or str).
+        """
+        with open('ivac_events.csv', 'a', newline='') as csvfile:
+            fieldnames = list(msg_value.keys())
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            csvfile.seek(0, 2)
+            if csvfile.tell() == 0:
+                writer.writeheader()
+
+            writer.writerow(msg_value)
+
+
+app = ToolMonitoring(
+    app_uuid='SPINDLE-MONITORING',
+    ksqlClient=KSQLDBClient("http://ksqldb-server:8088"),
+    bootstrap_servers="broker:29092"
+)
+app.run()
+
