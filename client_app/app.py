f20927da4e7ed7eb17d01e631b1db05952992777
camelia-d-e <cades413@ulaval.ca>
2025-06-19 13:28:59 +0000

    get devices in client_app

--- client_app/app.py
+++ client_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
+from openfactory_api_client import OpenFactoryAPIClient
 
 
 class ClientApp:
@@ -7,14 +8,20 @@ class ClientApp:
     def __init__(self):
         self.app = Flask(__name__)
         self.setup_routes()
+        self.api_client = OpenFactoryAPIClient(base_url="http://localhost:8000")
 
     def setup_routes(self):
         @self.app.route("/")
         def index():
-            return send_from_directory('static', 'index.html')
+            return render_template('index.html', devices=self.api_client.get_all_devices() )
+
+        @self.app.route("/devices/<device_uuid>/status")
+        def device_status(device_uuid):
+            pass
+
 
     def run(self):
-        self.app.run()
+        self.app.run(port=3000)
 
 
 if __name__ == "__main__":
