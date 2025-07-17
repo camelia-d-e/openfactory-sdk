3c6129342819d9daf48c46ed00ee79c4dec5ffec
camelia-d-e <cades413@ulaval.ca>
2025-07-17 14:55:18 +0000

    fix virtual cnc

--- openfactory/virtual/CNC/adapter.py
+++ openfactory/virtual/CNC/adapter.py
@@ -4,15 +4,15 @@ import os
 from mtcadapter.mtcdevices import MTCDevice
 from mtcadapter.adapters import MTCAdapter
 
-class Virtual_iVACToolPlus(MTCDevice):
+class Virtual_CNC(MTCDevice):
     """
     Virtual adapter for iVAC Tool Plus, which detects current on any electrical powered device.
     """
 
     MIN_TOGGLE_TIME = float(os.environ.get("MIN_TOGGLE_TIME", 5))
     MAX_TOGGLE_TIME = float(os.environ.get("MAX_TOGGLE_TIME", 10))
-    MAX_SPINDLE_SPEED = 25000.0
-    MIN_SPINDLE_SPEED = 10000.0
+    MAX_SPINDLE_SPEED = 25000
+    MIN_SPINDLE_SPEED = 10000
 
     def __init__(self):
         self._spindle_speed = 0
@@ -20,20 +20,23 @@ class Virtual_iVACToolPlus(MTCDevice):
 
     def read_data(self) -> dict:
         """Read and toggle tool states with random delay"""
+
         time.sleep(round(random.uniform(self.MIN_TOGGLE_TIME, self.MAX_TOGGLE_TIME), 2))
-        self.spindle_speed = random.randint(self.MIN_SPINDLE_SPEED, self.MAX_SPINDLE_SPEED)
-        if self.actuator_state == 'ACTIVE':
-            self.actuator_state = 'INACTIVE'
+        self._spindle_speed = random.randint(self.MIN_SPINDLE_SPEED, self.MAX_SPINDLE_SPEED)
+
+        if self._vacuum_status == 'ACTIVE':
+            self._vacuum_status = 'INACTIVE'
         else:
             self.actuator_state = 'ACTIVE'
+
         return {
-            'SpindleSpeed': self.spindle_speed,
-            'VacuumSystem': self.actuator_state
+            'spindle_speed': self._spindle_speed,
+            'vacuum_status': self._vacuum_status
         }
 
 
-class Virtual_iVACToolPlusAdapter(MTCAdapter):
-    device_class = Virtual_iVACToolPlus
+class Virtual_CNCAdapter(MTCAdapter):
+    device_class = Virtual_CNC
     adapter_port = int(os.environ.get("ADAPTER_PORT", 7878))
 
     def __init__(self):
@@ -48,7 +51,7 @@ class Virtual_iVACToolPlusAdapter(MTCAdapter):
 
 
 def main():
-    adapter = Virtual_iVACToolPlusAdapter()
+    adapter = Virtual_CNCAdapter()
     adapter.run()
 
 
